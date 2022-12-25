0f2f8f3240668e9ae5f9291ce10018d87e797180
ankita-barad <ankitabarad2111@gmail.com>
2022-12-25 21:12:46 +0530

    timepass

--- Variables.js
+++ Variables.js
@@ -1,4 +1,5 @@
 //Print "Masai School" in the console followed by "A Transformation in Education" in next line
 
-console.log("Masai School")
-console.log("Transformation in Education")
+console.log("Masai School");
+console.log("Transformation in Education");
+console.log("Hello Ankita");
